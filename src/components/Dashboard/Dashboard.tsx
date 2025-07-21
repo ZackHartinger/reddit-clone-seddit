8d7df2b8b0a39d58cae1a580649f07817ac793ea
ZackHartinger <zackhrtngr@gmail.com>
2025-07-20 21:04:02 -0700

    Removed React import statements causing errors when deploying to Netlify.

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import DashboardLink from './DashboardLink'
 
 const Dashboard = () => {
--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import PostHeader from './PostHeader'
 import PostBody from './PostBody'
 import PostFooter from './PostFooter'
